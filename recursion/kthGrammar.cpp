818b537dd25048d3e9a3368647ea5f3dcad9c281
Ayush0054 <ayush5aprill@gmail.com>
2023-07-30 14:39:52 +0530

    kthgrammar

---
+++ recursion/kthGrammar.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+    using namespace std;
+    class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==0||n==1){
+            return 0;
+        }
+        int mid = pow(2,n-1)/2;
+        if(k<=mid){
+            return kthGrammar(n-1,k);
+        }else{
+            return !kthGrammar(n-1,k-mid);
+        }
+    }
+};
+    int main(){
+    
+return 0;
+}
