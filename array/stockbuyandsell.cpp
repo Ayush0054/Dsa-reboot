bec3982b768b785511d58d4c2584e3a3fc87d8e5
Ayush0054 <ayush5aprill@gmail.com>
2023-09-18 19:16:28 +0530

    stock

---
+++ array/stockbuyandsell.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]>prices[i]){
+
+                int prf=prices[j]-prices[i];
+                maxi=max(maxi,prf);
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+optimal
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
+        int mini=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+          mini=min(prices[i],mini);
+          maxi=max(prices[i]-mini,maxi);
+        }
+        return maxi;
+    }
+};
